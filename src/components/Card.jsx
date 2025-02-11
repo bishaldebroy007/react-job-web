3fc8928e2d2649bd8f485411b41d9b2f1b60ec54
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-02-11 18:47:48 +0600

    OFC last for the day

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+
+
+const Card = ({ children }) => {
+    return (
+        <div className="bg-gray-100 p-6 rounded-lg shadow-emerald-50">
+            {children}
+        </div>
+    )
+}
+
+export default Card
--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,4 +1,4 @@
-
+import Cards from './Card';
 
 const HomeCards = () => {
     return (
